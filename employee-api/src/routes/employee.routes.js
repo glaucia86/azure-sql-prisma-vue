648808fde2ceb19f7184b2bdc2b502d73af56934
glaucia86 <glaucia_lemos86@hotmail.com>
2022-01-09 00:47:33 -0300

    creating the route createEmployee - 'employee.routes.js'

--- employee-api/src/routes/employee.routes.js
+++ employee-api/src/routes/employee.routes.js
@@ -11,4 +11,7 @@ const employeeController = require('../controllers/employee.controller');
 // ==> List All Employees: (GET): localhost:3001/api/v1/employees
 router.get('/employees', employeeController.listAllEmployees);
 
+// ==> Create a new Employee: (POST): localhost:3001/api/v1/employees
+router.post('/employees', employeeController.createEmployee);
+
 module.exports = router;
