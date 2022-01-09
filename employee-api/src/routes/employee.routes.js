5d12fb731bc2eb67e58428ccbe44083b12fe1f78
glaucia86 <glaucia_lemos86@hotmail.com>
2022-01-08 23:55:40 -0300

    developing the route GET All Employees - 'employee.routes.js'

--- employee-api/src/routes/employee.routes.js
+++ employee-api/src/routes/employee.routes.js
@@ -1,6 +1,14 @@
 /**
  * file: src/routes/employee.routes.js
  * description: file responsible for API routes
- * date: 01/03/2022
+ * date: 01/08/2022
  * author: Glaucia Lemos <@glaucia_lemos86>
  */
+
+const router = require('express-promise-router')();
+const employeeController = require('../controllers/employee.controller');
+
+// ==> List All Employees: (GET): localhost:3001/api/v1/employees
+router.get('/employees', employeeController.listAllEmployees);
+
+module.exports = router;
