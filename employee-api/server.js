8813627340bb640fd6824de47a11b7899585dfbb
glaucia86 <glaucia_lemos86@hotmail.com>
2022-01-08 22:35:49 -0300

    changing the Port number 'server.js'

--- employee-api/server.js
+++ employee-api/server.js
@@ -7,7 +7,7 @@
 
 const app = require('./src/app');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log('Application running on port ', port);
