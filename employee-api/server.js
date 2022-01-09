58b1a0ccb6199206e91b16393206c5d201c7181c
glaucia86 <glaucia_lemos86@hotmail.com>
2022-01-08 22:07:50 -0300

    developing the file 'server.js'

--- employee-api/server.js
+++ employee-api/server.js
@@ -1,6 +1,14 @@
 /**
- * file: app.js/index.js
+ * file: server.js
  * description: main file responsible for all configuration and execution of the Node.js Api Application
- * date: 01/03/2022
+ * date: 01/08/2022
  * author: Glaucia Lemos <@glaucia_lemos86>
  */
+
+const app = require('./src/app');
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('Application running on port ', port);
+});
