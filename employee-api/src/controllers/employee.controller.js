d9f8bed83227213a350c1e8fbb814134bbd9df5f
glaucia86 <glaucia_lemos86@hotmail.com>
2022-01-08 23:57:43 -0300

    developing the logic - Get All Employee - 'employee.controller.js'

--- employee-api/src/controllers/employee.controller.js
+++ employee-api/src/controllers/employee.controller.js
@@ -1,6 +1,22 @@
 /**
  * file: src/controllers/employee.controller.js
  * description: file responsible for the CRUD logic (API - Employee)
- * date: 01/03/2022
+ * date: 01/08/2022
  * author: Glaucia Lemos <@glaucia_lemos86>
  */
+
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+exports.listAllEmployees = async (req, res) => {
+  try {
+    const employees = await prisma.employee.findMany({});
+    res.status(200).send(employees);
+  } catch (error) {
+    console.log('listAllEmployees', error);
+    res.status(500).send({
+      message: 'Occur an error!',
+    });
+  }
+};
