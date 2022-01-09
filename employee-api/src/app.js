83411097e40a65717b7b1619f82cddcd261a6065
glaucia86 <glaucia_lemos86@hotmail.com>
2022-01-08 22:26:41 -0300

    developing the file app.js

--- employee-api/src/app.js
+++ employee-api/src/app.js
@@ -1,6 +1,24 @@
 /**
  * file: src/app.js
  * description: file responsible for connecting to the 'server.js' file
- * date: 01/03/2022
+ * date: 01/08/2022
  * author: Glaucia Lemos <@glaucia_lemos86>
  */
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// ==> API's Routes
+const index = require('./routes/index');
+// const employeeRoute = require('./routes/employee.routes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.json({ type: 'application/vnd.api+json' }));
+app.use(cors());
+
+app.use(index);
+
+module.exports = app;
