cff2aea6cd8f753b96b052dbffc37467225f28f6
glaucia86 <glaucia_lemos86@hotmail.com>
2022-01-11 20:31:34 -0300

    adding the plugin and packages in the 'main.js' file

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
+import VueSweetalert2 from 'vue-sweetalert2';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueSweetalert2);
 
 new Vue({
   router,
