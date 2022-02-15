04644d0c1a2f75484b77acb7c54ea17f17f46717
ruheni <aleruheni@outlook.com>
2022-02-15 17:04:49 +0100

    add starter code

--- employee-api/src/routes/employee.routes.js
+++ employee-api/src/routes/employee.routes.js
@@ -7,4 +7,7 @@ router.get('/employees', employeeController.listAllEmployees);
 // ==> Create a new Employee: (POST): localhost:3001/api/v1/employees
 router.post('/employees', employeeController.createEmployee);
 
+// ==> List 'Employee' by Id: (GET): localhost:3001/api/v1/employees/:id
+router.get('/employees/:id', employeeController.findEmployeeById);
+
 module.exports = router;
