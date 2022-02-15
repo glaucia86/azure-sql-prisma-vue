65fff0b1577b121a6c7384c0e653e22c2c634259
ruheni <aleruheni@outlook.com>
2022-02-15 15:30:15 +0100

    delete unneccessary directories

--- employee-api/src/config/database.config.js
+++
@@ -1,6 +0,0 @@
-/**
- * file: src/database/database.config.js
- * description: file responsible for local configuration database
- * date: 01/03/2022
- * author: Glaucia Lemos <@glaucia_lemos86>
- */
