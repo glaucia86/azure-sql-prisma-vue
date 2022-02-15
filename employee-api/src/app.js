3bd44c8b56eee2a13d417dbceec58100848e3e43
ruheni <aleruheni@outlook.com>
2022-02-15 15:56:49 +0100

    routes setup branch setup improvement

--- employee-api/src/app.js
+++ employee-api/src/app.js
@@ -8,7 +8,4 @@ app.use(express.json());
 app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use(cors());
 
-app.use(index);
-app.use('/api/v1/', employeeRoute);
-
 module.exports = app;
