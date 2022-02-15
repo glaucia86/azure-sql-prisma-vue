40322640c77cf72cf687a8ed098824261f10bf2b
ruheni <aleruheni@outlook.com>
2022-02-15 17:10:05 +0100

    add starter code

--- employee-api/src/controllers/employee.controller.js
+++ employee-api/src/controllers/employee.controller.js
@@ -1,6 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
-
-const prisma = new PrismaClient();
+const prisma = require('../config/prisma-client.config');
 
 exports.listAllEmployees = async (req, res) => {
   try {
