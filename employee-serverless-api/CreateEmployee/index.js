ba2e036c39b42e4428b9536e9110c1f16e40db03
glaucia86 <glaucia_lemos86@hotmail.com>
2022-02-21 18:36:17 -0300

    updating the 'CreateEmployee'

--- employee-serverless-api/CreateEmployee/index.js
+++ employee-serverless-api/CreateEmployee/index.js
@@ -8,26 +8,25 @@
 const prisma = require('../shared/prisma');
 const handleError = require('../shared/error');
 
-module.exports = async function(context, req) {
-  const { name, job_role, salary, employee_registration } = req.body;
-
+module.exports = async function (context, req) {
   try {
+    const { name, job_role, salary, employee_registration } = req.body;
+
     const employee = await prisma.employee.create({
       data: {
         name,
         job_role,
         salary,
         employee_registration: parseInt(employee_registration),
-      }
-    })
+      },
+    });
 
     return {
-      status: 200,
-      body: employee
-    }
+      status: 201,
+      body: employee,
+    };
   } catch (error) {
     context.log('Error to create a new Employee');
     return handleError(500, error, context);
   }
-}
-
+};
