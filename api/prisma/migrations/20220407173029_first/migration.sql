d8cd4a13cafb0d387c482b7fa5ba2a6abcd69482
glaucia86 <glaucia_lemos86@hotmail.com>
2022-04-07 14:39:34 -0300

    updating the schema.prisma file

---
+++ api/prisma/migrations/20220407173029_first/migration.sql
@@ -0,0 +1,26 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- CreateTable
+CREATE TABLE [dbo].[Employee] (
+    [employee_id] INT NOT NULL IDENTITY(1,1),
+    [name] NVARCHAR(100) NOT NULL,
+    [job_role] NVARCHAR(100) NOT NULL,
+    [salary] DECIMAL(12,2) NOT NULL,
+    [employee_registration] INT NOT NULL,
+    CONSTRAINT [Employee_pkey] PRIMARY KEY ([employee_id])
+);
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
