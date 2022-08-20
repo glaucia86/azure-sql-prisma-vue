4b18b415c02719a366467c1e06a1bc3adf36a2f0
glaucia86 <glaucia_lemos86@hotmail.com>
2022-08-20 19:17:24 -0300

    updating minor and patchs packages - client

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -8,19 +8,18 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: () => import('../components/pages/create-employee/CreateEmployeeComponent'),
+    component: () => import('../components/pages/create-employee/CreateEmployeeComponent.vue'),
   },
   {
     path: '/list-employees',
     name: 'list',
-    component: () => import('../components/pages/list-employee/ListEmployeeComponent'),
+    component: () => import('../components/pages/list-employee/ListEmployeeComponent.vue'),
   },
   {
     path: '/edit-employee/:id',
     name: 'update',
-    component: () => import('../components/pages/edit-employee/EditEmployeeComponent'),
+    component: () => import('../components/pages/edit-employee/EditEmployeeComponent.vue'),
   },
-
 ];
 
 const router = new VueRouter({
